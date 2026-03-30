c7575af30582c6f2da5667ae220fb1e0a7ffe7f9
Brian dos Reis <brazdore@gmail.com>
2026-03-30 01:49:07 -0300

    Adiciona scripts de atualização de coordenadas para NPCs

---
+++ database/V09__fix_rashid_coords_tibiamaps.sql
@@ -0,0 +1,7 @@
+UPDATE silkroad.npc_locations SET coord_x = 32327, coord_y = 31782, coord_z = 6 WHERE npc_id = 1 AND weekday = 'Sunday';
+UPDATE silkroad.npc_locations  SET coord_x = 32210, coord_y = 31157, coord_z = 7 WHERE npc_id = 1 AND weekday = 'Monday';
+UPDATE silkroad.npc_locations  SET coord_x = 32303, coord_y = 32834, coord_z = 7 WHERE npc_id = 1 AND weekday = 'Tuesday';
+UPDATE silkroad.npc_locations  SET coord_x = 32577, coord_y = 32752, coord_z = 7 WHERE npc_id = 1 AND weekday = 'Wednesday';
+UPDATE silkroad.npc_locations  SET coord_x = 33069, coord_y = 32885, coord_z = 6 WHERE npc_id = 1 AND weekday = 'Thursday';
+UPDATE silkroad.npc_locations  SET coord_x = 33238, coord_y = 32482, coord_z = 7 WHERE npc_id = 1 AND weekday = 'Friday';
+UPDATE silkroad.npc_locations  SET coord_x = 33175, coord_y = 31800, coord_z = 6 WHERE npc_id = 1 AND weekday = 'Saturday';
---
+++ database/V10__update_djinns_npc_coords.sql
@@ -0,0 +1,15 @@
+UPDATE silkroad.npc_locations
+SET coord_x = 33104, coord_y = 32520, coord_z = 2
+WHERE npc_id = 2;
+
+UPDATE silkroad.npc_locations
+SET coord_x = 33108, coord_y = 32524, coord_z = 4
+WHERE npc_id = 3;
+
+UPDATE silkroad.npc_locations
+SET coord_x = 33045, coord_y = 32619, coord_z = 2
+WHERE npc_id = 4;
+
+UPDATE silkroad.npc_locations
+SET coord_x = 33048, coord_y = 32620, coord_z = 5
+WHERE npc_id = 5;
